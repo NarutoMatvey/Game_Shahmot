8b2676ca75de75247f66c8960ed4c9b018d53b1e
DESKTOP-6FPT8OC\NarutoMatvey <NarutoMatvey@mail.ru>
2017-09-29 03:06:02 +0300

    Раздобыл размер карты

--- Client_Labirint.py
+++ Client_Labirint.py
@@ -3,6 +3,12 @@ import socket
 
 conn = socket.socket()
 
-conn.connect(("127.0.0.1", 5000))
+conn.connect(("127.0.0.1", 7000))
 
-hod = conn.recv(1024)
+# Размер массива
+temp = str(conn.recv(5))
+temp = temp[2:len(temp)-1]
+razmer = []
+for i in temp.split(','):
+    razmer += [int(i)]
+
