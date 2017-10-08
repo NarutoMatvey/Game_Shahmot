ace0c0d2f23170ef985d262665d6751e4fa86ef1
DESKTOP-6FPT8OC\NarutoMatvey <NarutoMatvey@mail.ru>
2017-10-08 21:07:16 +0300

    Дописал второго клиента. Изменил Сервер. И дополнил карты.

--- Client_Labirint.py
+++ Client_Labirint.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import socket
 
+otvet = []
 conn = socket.socket()
 
 conn.connect(("127.0.0.1", 7000))
+
+player = conn.recv(1)
 
 while 1:
     Next_End = conn.recv(1)
@@ -11,7 +14,9 @@ while 1:
         print('The End!\nYou Win!')
         break
     # Размер массива
-    temp_dan = str(conn.recv(5))
+
+    temp_dan = str(conn.recv(3))
+    print(temp_dan)
     temp_dan = temp_dan[2:len(temp_dan)-1]
     razmer = []
     print()
@@ -37,24 +42,55 @@ while 1:
 
     # Игра началась
     while 1:
-        Naprav = input("Выбирайте направление: ").strip()
+        if player == b'1':
+            #Первый пользователь
+            Naprav = input("Выбирайте направление: ").strip()
 
-        if 'R' in Naprav or 'r' in Naprav:
-            conn.send(b'r')
-        elif 'L' in Naprav or 'l' in Naprav:
-            conn.send(b'l')
-        elif 'T' in Naprav or 't' in Naprav:
-            conn.send(b't')
-        elif 'D' in Naprav or 'd' in Naprav:
-            conn.send(b'd')
+            if 'R' in Naprav or 'r' in Naprav:
+                conn.send(b'r')
+            elif 'L' in Naprav or 'l' in Naprav:
+                conn.send(b'l')
+            elif 'T' in Naprav or 't' in Naprav:
+                conn.send(b't')
+            elif 'D' in Naprav or 'd' in Naprav:
+                conn.send(b'd')
+            else:
+                continue
+            temp_peremesh = conn.recv(3)
         else:
-            continue
+            #Второй пользователь
+            Naprav = conn.recv(1)
 
-        temp_peremesh = conn.recv(3)
+            if b'r' == Naprav:
+                print('Right')
+            elif b'l' == Naprav:
+                print('Left')
+            elif b't' == Naprav:
+                print('Top')
+            elif b'd' == Naprav:
+                print('Down')
+
+            while 1:
+                while 1:
+                    Step = input("Сколько клеток пройти?: ").strip()
+                    try:
+                        if Step > '0' and int(Step) < len(Karta_podgruz):
+                            break
+                    except:
+                        continue
+                conn.send(Step.encode())
+                temp_peremesh = conn.recv(3)
+                if temp_peremesh == b'Non':
+                    print('Много!!!')
+                    continue
+                break
+
         if temp_peremesh == b'Win':
             print('You Win!')
+            if player == b'2':
+                otvet += [conn.recv(1)]
+                print(otvet)
             break
-
         temp_peremesh = str(temp_peremesh)
         temp_peremesh = temp_peremesh[2:len(temp_peremesh)-1]
         temp_peremesh = temp_peremesh.split(',')
--- Server_Laberint_One_Polzovatel.py
+++ Server_Laberint_One_Polzovatel.py
@@ -3,12 +3,13 @@ import socket  # Подключение библиотеки
 import karts  # Библиотека с картами
 import random  # Библиотека псевда рандома
 
-
+global otvet
 # Функции
 
 # Блок взаимодействия с картой
-def TrevalKarts(karta, conn):
+def TrevalKarts(karta, conn, player):
     # Отправляет размер карты
+    global otvet
     conn.send((str(len(karta)) + ',' + str(len(karta[0]))).encode('utf-8'))
 
     # Отправляет карту
@@ -17,18 +18,26 @@ def TrevalKarts(karta, conn):
             if karta[i][j] == 'I':
                 goriz = j
                 vertical = i
-            conn.send(karta[i][j].encode('utf-8'))
+            elif 'a' <= karta[i][j] <= 'z':
+                otvet = karta[i][j]
+                print(otvet)
+                karta[i][j] = 'E'
+            if player == 2:
+                if karta[i][j] == 'I':
+                    conn.send(karta[i][j].encode('utf-8'))
+                else:
+                    conn.send(b'.')
+            else:
+                conn.send(karta[i][j].encode('utf-8'))
     return goriz, vertical
 
 
 # Проверка возможности
-def proverka_na_cctenu(karta, v, g, shag1=0, shag2=0):
-    print('#0', v, g, shag1, shag2)
+def proverka_na_cctenu(karta, v, g, conn2 = None, shag1=0, shag2=0):
+
     light = len(karta)
-    print('#1', light)
     go = 1
     while 1:
-        print('#2', v + shag1, g + shag2)
         if 0 <= v + shag1 < light and 0 <= g + shag2 < light:
             if karta[v + shag1][g + shag2] == '.':
                 if shag1 == 0:
@@ -41,17 +50,12 @@ def proverka_na_cctenu(karta, v, g, shag1=0, shag2=0):
                         shag1 = -light
                     else:
                         shag1 = light
-
-                print('#3', shag1, shag2)
             else:
                 temp1 = v + shag1
                 temp2 = g + shag2
-                print('#4', temp1, temp2)
                 if shag2 < 0 or shag1 < 0:
                     go = -1
-                print('#5', go)
                 for i in range(v, temp1, go):  # Неправильные циклы
-                    print('#', i, karta[i][g])
                     if karta[i][g] == '.':
                         if shag1 < 0:
                             shag1 = -light
@@ -60,7 +64,6 @@ def proverka_na_cctenu(karta, v, g, shag1=0, shag2=0):
                         break
 
                 for i in range(g, temp2, go):
-                    print('#', i, karta[v][i])
                     if karta[v][i] == '.':
                         if shag2 < 0:
                             shag2 = -light
@@ -70,86 +73,106 @@ def proverka_na_cctenu(karta, v, g, shag1=0, shag2=0):
 
                 if shag1 != light and shag2 != light and shag1 != -light and shag2 != -light:
                     while v != temp1:
-                        print(karta[v][g])
                         karta[v][g] = '+'
-                        print(karta[v][g])
                         v += go
 
                     while g != temp2:
-                        print(karta[v][g])
                         karta[v][g] = '+'
-                        print(karta[v][g])
                         g += go
 
-                    if karta[v][g] == 'e':
+                    if karta[v][g] == 'E':
                         karta[v][g] = 'I'
                         return 'Win'
                     else:
-                        print(karta[v][g])
                         karta[v][g] = 'I'
-                        print(karta[v][g])
                         return (str(v) + ',' + str(g)).encode('utf-8')
 
         else:
             # Если смещение на такое число несуществует
+            conn2.send(b'Non')
             if shag1 == 0:
                 if shag2 < 0:
-                    shag2 = random.randint(1, light - 1)
+                    shag2 = str(conn2.recv(1))
+                    shag2 = shag2[2:len(shag2)-1]
+                    shag2 = int(shag2)
                     shag2 = -shag2
                 else:
-                    shag2 = random.randint(1, light - 1)
-                print('#9', shag2)
+                    shag2 = str(conn2.recv(1))
+                    shag2 = shag2[2:len(shag2)-1]
+                    shag2 = int(shag2)
             elif shag1 < 0:
-                shag1 = random.randint(1, light - 1)
+                shag1 = str(conn2.recv(1))
+                shag1 = shag1[2:len(shag1)-1]
+                shag1 = int(shag1)
                 shag1 = -shag1
-                print('#10', shag1)
             else:
-                shag1 = random.randint(1, light - 1)
-                print('#11', shag1)
+                shag1 = str(conn2.recv(1))
+                shag1 = shag1[2:len(shag1)-1]
+                shag1 = int(shag1)
 
+karta_mass = karts.Karts()
+karta_mass = karta_mass.Randomaiz()
 
 # Можно будет выделить в блок
 sock = socket.socket()  # Создание сокета
 
 sock.bind(('localhost', 7000))  # Настраиваю определённый порт для общения с клиентом
 
-sock.listen(1)  # Создаю очередь пользователей
+sock.listen(2)  # Создаю очередь пользователей
 
 conn, adr = sock.accept()  # Ожидаю подключения
+conn.send(b'1')
+print("Первый игрок!")
 
-karta_mass = karts.Karts()
-karta_mass = karta_mass.Randomaiz()
-
+conn2, adr2 = sock.accept()
+conn2.send(b'2')
+print("Второй игрок!")
 # Игра началась
 for karta in karta_mass:
     conn.send(b'N')
-
-    goriz, vertical = TrevalKarts(karta, conn)
-
+    conn2.send(b'N')
+    goriz, vertical = TrevalKarts(karta, conn, 1)
+    TrevalKarts(karta, conn2, 2)
     while 1:
 
         naprawlenie = conn.recv(1)
-        shag = random.randint(1, len(karta) - 1)
-        print("Новый этап \n")
 
         if naprawlenie == b'r' and goriz + 1 < len(karta[0]) and karta[vertical][goriz + 1] != '.':
-            winer = proverka_na_cctenu(karta, vertical, goriz, shag2=shag)
+            conn2.send(naprawlenie)
+            shag = str(conn2.recv(1))
+            shag = shag[2:len(shag) - 1]
+            shag = int(shag)
+            winer = proverka_na_cctenu(karta, vertical, goriz, conn2, shag2=shag)
         elif naprawlenie == b'l' and goriz - 1 >= 0 and karta[vertical][goriz - 1] != '.':
-            winer = proverka_na_cctenu(karta, vertical, goriz, shag2=-shag)
+            conn2.send(naprawlenie)
+            shag = str(conn2.recv(1))
+            shag = shag[2:len(shag)-1]
+            shag = int(shag)
+            winer = proverka_na_cctenu(karta, vertical, goriz, conn2, shag2=-shag)
         elif naprawlenie == b'd' and vertical + 1 < len(karta) and karta[vertical + 1][goriz] != '.':
-            winer = proverka_na_cctenu(karta, vertical, goriz, shag1=shag)
+            conn2.send(naprawlenie)
+            shag = str(conn2.recv(1))
+            shag = shag[2:len(shag) - 1]
+            shag = int(shag)
+            winer = proverka_na_cctenu(karta, vertical, goriz, conn2, shag1=shag)
         elif naprawlenie == b't' and vertical - 1 >= 0 and karta[vertical - 1][goriz] != '.':
-            winer = proverka_na_cctenu(karta, vertical, goriz, shag1=-shag)
+            conn2.send(naprawlenie)
+            shag = str(conn2.recv(1))
+            shag = shag[2:len(shag) - 1]
+            shag = int(shag)
+            winer = proverka_na_cctenu(karta, vertical, goriz, conn2, shag1=-shag)
         else:
             conn.send((str(vertical) + ',' + str(goriz)).encode('utf-8'))
             continue
 
-        print('# ', winer)
         if winer == "Win":
             conn.send(b'Win')
+            conn2.send(b'Win')
+            conn2.send(otvet.encode())
             break
         else:
             conn.send(winer)
+            conn2.send(winer)
             winer = str(winer)
             winer = winer[2:len(winer) - 1]
             winer = winer.split(',')
@@ -158,5 +181,7 @@ for karta in karta_mass:
             winer = ''
 
 conn.send(b'E')
+conn2.send(b'E')
 conn.close()
+conn2.close()
 sock.close()
